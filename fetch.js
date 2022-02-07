928fdaa293678f4c241d66a352c9fec1a775daa1
Sandeep Ramgolam <sandeep@ramgolam.com>
2022-02-07 15:43:50 +0400

    added exit code

--- fetch.js
+++ fetch.js
@@ -1,6 +1,7 @@
 import scraper from 'table-scraper';
 import { cleanup } from "./utils.js"
 import fs from 'fs'
+import { exit } from 'process';
 const URL = 'https://www.stcmu.com/ppm/retail-prices'
 
 scraper
@@ -11,6 +12,9 @@ scraper
         let stringified = JSON.stringify(processsed);
         fs.writeFileSync('./data/latest.json', stringified);
         fs.writeFileSync(`./data/history/${filname_with_timestamp}.json`, stringified);
-    });
-
+        exit(0);
+    }).catch(function (err) {
+        console.error(err);
+        exit(1);
+    })
 
